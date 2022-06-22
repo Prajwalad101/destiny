802ce16c02c367c1f858c69652dc7a61d450c6cc
Prajwal Adhikari <prajwalad101@gmail.com>
2022-06-22 16:13:07 +0545

    refactor: make minor changes to main layout

--- client/pages/index.tsx
+++ client/pages/index.tsx
@@ -2,13 +2,13 @@ import type { NextPage } from 'next';
 
 const Home: NextPage = () => {
   return (
-    <div className="mt-10">
+    <div className="mt-7 md:mt-12">
       <div className="relative z-10">
-        <h1 className="max-w-sm font-merriweather text-4xl font-black leading-tight tracking-wider md:max-w-xl md:text-[45px] md:leading-snug md:text-white">
+        <h1 className="max-w-sm font-merriweather text-4xl font-black leading-tight tracking-wider sm:max-w-xl sm:text-[40px] md:text-[45px] md:leading-[69px] md:text-white">
           Find and support local businesses
         </h1>
       </div>
-      <div className="absolute inset-0 h-[95vh] bg-main-img bg-cover bg-no-repeat"></div>
+      <div className="absolute inset-0 hidden h-[95vh] bg-main-img bg-cover bg-no-repeat md:block"></div>
     </div>
   );
 };
