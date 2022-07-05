9d6bdd2dee446ba4329f30b1a0805793d9e8ad77
Prajwal Adhikari <prajwalad101@gmail.com>
2022-07-05 18:35:53 +0545

    fix: prevent errors when rendering business cards

--- client/components/cards/business/BusinessCard.tsx
+++ client/components/cards/business/BusinessCard.tsx
@@ -1,53 +1,53 @@
+import { IBusiness } from '@destiny/types';
 import Image from 'next/image';
 import { FaQuoteLeft } from 'react-icons/fa';
 import RatingIcons from '../../icons/ratings/RatingIcons';
 
-interface IReviews {
-  id: number;
-  review: string;
+interface IBusinessCard {
+  business: IBusiness;
 }
 
-export interface IBusinessCard {
-  name: string;
-  location: string;
-  image: string;
-  numReviews: number;
-  avgRating: number;
-  status?: string;
-  reviews: IReviews[];
-  type?: string[];
-  isOpen?: boolean;
-}
-function BusinessCard(props: IBusinessCard) {
+function BusinessCard({ business }: IBusinessCard) {
   return (
     <div className="font-rubik sm:flex">
       {/* Image */}
       <div className="relative h-[200px] w-full sm:w-56">
         <Image
-          src={props.image}
-          alt={props.name}
+          src={business.images[0]}
+          alt={business.name}
           layout="fill"
           objectFit="cover"
         />
       </div>
       {/* Body */}
       <div className="grow border-x-2 border-b-2 p-2 sm:border-l-0 sm:border-t-2 sm:p-3">
-        <h3 className="mb-2 text-lg font-medium">{props.name}</h3>
+        <h3 className="mb-2 text-lg font-medium">{business.name}</h3>
         <div className="mb-2 flex items-center gap-4">
           <div className="flex items-center gap-3">
-            <RatingIcons avgRating={props.avgRating} />
-            <p className="text-sm text-secondarytext">({props.numReviews})</p>
+            <RatingIcons avgRating={business.rating} />
+            <p className="text-sm text-secondarytext">
+              {/* ({business.reviews.length}) */}
+              54
+            </p>
           </div>
-          <p className="text-[15px] font-medium text-secondarytext">
-            {props.isOpen ? 'Open now' : 'Closed'}
-          </p>
+          {/* TODO: CALCULATE ISOPEN OR CLOSED BASED ON BUSINESS HOURS */}
+
+          {/* <p className="text-[15px] font-medium text-secondarytext">
+            {business.isOpen ? 'Open now' : 'Closed'}
+          </p> */}
         </div>
-        <p className="mb-4 text-sm text-secondarytext">{props.location}</p>
+        <p className="mb-4 text-sm text-secondarytext">
+          {business.location.address}
+        </p>
         {/* Horizontal Line */}
         <div className="mb-3 hidden border-[1px] sm:block"></div>
         {/* Reviews */}
-        {props.reviews.map((review) => (
-          <div key={review.id} className="mb-2 flex items-center gap-3">
+        {/* TODO: IMPLEMENT REVIEWS */}
+        {business.reviews.map((review) => (
+          <div
+            key={review._id.toString()}
+            className="mb-2 flex items-center gap-3"
+          >
             <FaQuoteLeft size={15} />
             <p className="overflow-hidden text-ellipsis whitespace-nowrap text-sm">
               &quot;{review.review}&quot;
