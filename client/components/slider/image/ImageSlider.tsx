f6a0009c5f0f0dcd91999cc105aed1c43bc107b4
Prajwal Adhikari <prajwalad101@gmail.com>
2022-07-07 15:22:12 +0545

    fix: prevent image slider buttons from referencing other images

--- client/components/slider/image/ImageSlider.tsx
+++ client/components/slider/image/ImageSlider.tsx
@@ -1,10 +1,12 @@
+import { Types } from 'mongoose';
 import Image from 'next/image';
 import { useRef } from 'react';
 import { BsArrowLeftCircleFill, BsArrowRightCircleFill } from 'react-icons/bs';
 import { useContainerDimensions } from '../../../hooks/lib/useContainerDimensions';
 
-export interface IImageSlider {
+export interface ImageSliderProps {
   images: string[];
+  id: Types.ObjectId;
 }
 
 const slideLeft = (id: string, width: number) => {
@@ -17,22 +19,23 @@ const slideRight = (id: string, width: number) => {
   slider.scrollLeft = slider.scrollLeft + width;
 };
 
-function ImageSlider({ images }: IImageSlider) {
+function ImageSlider({ id, images }: ImageSliderProps) {
   const componentRef = useRef<HTMLDivElement>(null);
   const { width } = useContainerDimensions(componentRef);
+  const businessId = id.toString();
 
   return (
     <>
       <div className="relative w-full sm:w-56">
         <button
           className="absolute top-[45%] left-2 z-10 text-white"
-          onClick={() => slideLeft('scroll', width)}
+          onClick={() => slideLeft(businessId, width)}
         >
           <BsArrowLeftCircleFill size={25} />
         </button>
         <button
           className="absolute top-[45%] right-3 z-10 text-white"
-          onClick={() => slideRight('scroll', width)}
+          onClick={() => slideRight(businessId, width)}
         >
           <BsArrowRightCircleFill size={25} />
         </button>
@@ -40,7 +43,7 @@ function ImageSlider({ images }: IImageSlider) {
         {/* TODO: Make the id unique for every slider component */}
         <div
           className="hide-scrollbar relative flex h-[200px] overflow-hidden scroll-smooth"
-          id="scroll"
+          id={businessId}
           ref={componentRef}
         >
           {images.map((image, index) => (
