e13835556c793b60d26149919fa276195a8c9b40
Prajwal Adhikari <prajwalad101@gmail.com>
2022-07-13 17:00:06 +0545

    fix(checkbox): issue when selecting checkbox

--- client/components/checkbox/Checkbox.stories.tsx
+++ client/components/checkbox/Checkbox.stories.tsx
@@ -1,10 +1,16 @@
+import { ComponentMeta, ComponentStory } from '@storybook/react';
 import Checkbox from './Checkbox';
 
 export default {
   title: 'components/Checkbox',
   component: Checkbox,
-};
+} as ComponentMeta<typeof Checkbox>;
 
-const Template = () => <Checkbox>Custom Box</Checkbox>;
+const Template: ComponentStory<typeof Checkbox> = (args) => (
+  <Checkbox {...args} />
+);
 
 export const Base = Template.bind({});
+Base.args = {
+  filterName: 'Custom Component',
+};
--- client/components/checkbox/Checkbox.tsx
+++ client/components/checkbox/Checkbox.tsx
@@ -1,23 +1,23 @@
 interface CheckboxProps {
-  children: React.ReactNode;
+  filterName: string;
 }
 
-function Checkbox({ children }: CheckboxProps) {
+function Checkbox({ filterName }: CheckboxProps) {
   return (
     <div>
       <input
         className="inp-cbx"
-        id="cbx"
+        id={filterName}
         type="checkbox"
         style={{ display: 'none' }}
       />
-      <label className="cbx" htmlFor="cbx">
+      <label className="cbx" htmlFor={filterName}>
         <span>
           <svg width="12px" height="10px" viewBox="0 0 12 10">
             <polyline points="1.5 6 4.5 9 10.5 1"></polyline>
           </svg>
         </span>
-        <span>{children}</span>
+        <span className="capitalize">{filterName}</span>
       </label>
     </div>
   );
