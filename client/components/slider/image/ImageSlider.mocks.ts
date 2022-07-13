1c0f659860c838e76c3f8391f75a0190c30ac3c9
Prajwal Adhikari <prajwalad101@gmail.com>
2022-07-13 21:59:12 +0545

    fix: modify mock data to prevent type error

--- client/components/slider/image/ImageSlider.mocks.ts
+++ client/components/slider/image/ImageSlider.mocks.ts
@@ -1,4 +1,3 @@
-import mongoose from 'mongoose';
 import { ImageSliderProps } from './ImageSlider';
 
 const base: ImageSliderProps = {
@@ -6,7 +5,6 @@ const base: ImageSliderProps = {
     '/images/business/shrestha-swimming-pool/1.jpg',
     '/images/business/shrestha-swimming-pool/2.jpg',
   ],
-  id: new mongoose.Types.ObjectId('24080980234'),
 };
 
 export const mockImageSliderProps = {
