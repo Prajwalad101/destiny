eb63d3b824fcf236a17b884e70bac920f5344539
Prajwal Adhikari <prajwalad101@gmail.com>
2022-07-13 15:24:17 +0545

    feat: implement searchFilter component on search page

--- client/pages/search/business/index.tsx
+++ client/pages/search/business/index.tsx
@@ -4,8 +4,10 @@ import { dehydrate, QueryClient } from 'react-query';
 import AppLayout from '../../../components/layout/app/AppLayout';
 import NavLayout from '../../../components/layout/navigation/NavLayout';
 import ProviderLayout from '../../../components/layout/provider/ProviderLayout.';
+import SearchFilter from '../../../components/search-filter/SearchFilter';
 import BusinessListSection from '../../../components/sections/business-list/BusinessListSection';
 import SortItems from '../../../components/sort/SortItems';
+import searchFilterData from '../../../data/searchFilter.data';
 import { sortItemData } from '../../../data/sortBusiness.data';
 import { fetchBusinesses } from '../../../hooks/business/useBusinesses';
 import { NextPageWithLayout } from '../../_app';
@@ -18,21 +20,23 @@ const SearchBusiness: NextPageWithLayout = () => {
 
   return (
     <div className="mt-5 flex gap-8 md:mt-10">
-      <div className="hidden h-[600px] w-[400px] bg-gray-300 lg:block" />
+      <SearchFilter filterOption={searchFilterData.resturants} />
       <div className="min-w-0 grow">
-        <div className="mb-7 flex flex-col justify-between gap-x-3 gap-y-4 sm:mr-10 sm:flex-row sm:items-center md:mb-10">
-          {/* Heading */}
-          <h2 className="font-merriweather text-2xl font-bold">
-            Top <span className="capitalize">{name}</span> in{' '}
-            <span className="capitalize">{city}</span>
-          </h2>
-          {/* Sort Menu */}
-          <div className="w-72">
-            <SortItems
-              sortItemData={sortItemData}
-              selectedSort={selectedSort}
-              setSelectedSort={setSelectedSort}
-            />
+        <div className="mb-7 sm:mr-5 md:mb-10">
+          <div className="flex flex-col justify-between gap-4 sm:flex-row sm:items-center">
+            {/* Heading */}
+            <h2 className="font-merriweather text-2xl font-bold">
+              Top <span className="capitalize">{name}</span> in{' '}
+              <span className="capitalize">{city}</span>
+            </h2>
+            {/* Sort Menu */}
+            <div className="w-72">
+              <SortItems
+                sortItemData={sortItemData}
+                selectedSort={selectedSort}
+                setSelectedSort={setSelectedSort}
+              />
+            </div>
           </div>
         </div>
         <BusinessListSection selectedSort={selectedSort} />
