8d5928b2c5a3bdff5b7200796b2b01cd661f36e2
Prajwal Adhikari <prajwalad101@gmail.com>
2022-07-20 15:03:16 +0545

    refactor: extract business info into seperate component

---
+++ client/components/sections/business-info/BusinessInfo.section.tsx
@@ -0,0 +1,64 @@
+import { IBusiness } from '@destiny/types';
+import Image from 'next/image';
+import { checkIsOpen } from '../../../utils/api';
+import { truncateText } from '../../../utils/text';
+import RatingIcons from '../../icons/ratings/RatingIcons';
+import ImageScroll from '../../image/scroll/ImageScroll';
+
+interface BusinessInfoProps {
+  business: IBusiness;
+}
+
+function BusinessInfo({ business }: BusinessInfoProps) {
+  return (
+    <div className="mt-4 font-rubik">
+      <div className="relative mb-4 h-[200px] w-full">
+        <Image
+          alt={business.name}
+          src={business.images[0]}
+          layout="fill"
+          objectFit="cover"
+        />
+      </div>
+      <h4 className="mb-2 text-xl font-medium">{business.name}</h4>
+      {/* Rating, Num Reviews */}
+      <div className="mb-2 flex items-center gap-4">
+        <RatingIcons avgRating={business.avgRating} />
+        <span>{business.rating_count} reviews</span>
+        <span className="font-medium">
+          {checkIsOpen(business.businessHours) ? 'Open Now' : 'Closed'}
+        </span>
+      </div>
+      {/* Address */}
+      <p className="mb-3 text-secondarytext">{business.location.address}</p>
+      {/* Tags */}
+      <div className="mb-3 flex gap-2">
+        <div className="w-max cursor-pointer rounded-sm bg-gray-200 px-3 py-[2px] hover:bg-gray-300">
+          <span className="text-sm capitalize text-secondarytext">
+            {business.tags[0]}
+          </span>
+        </div>
+        <div className="w-max cursor-pointer rounded-sm bg-gray-200 px-3 py-[2px] hover:bg-gray-300">
+          <span className="text-sm capitalize text-secondarytext">
+            {business.tags[1]}
+          </span>
+        </div>
+      </div>
+      {/* Description */}
+      {/* <p>{business.description}</p> */}
+      <p className="mb-7 text-gray-600">
+        {truncateText(
+          'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Enim, nihil dignissimos? Odio vitae dolor quasi eum eos sequi asperiores unde.',
+          20
+        )}
+        <span className="cursor-pointer text-primaryred  hover:text-red-600">
+          {' '}
+          Read More
+        </span>
+      </p>
+      <ImageScroll businessId={business._id} images={business.images} />
+    </div>
+  );
+}
+
+export default BusinessInfo;
--- client/pages/search/business/[businessId].tsx
+++ client/pages/search/business/[businessId].tsx
@@ -1,18 +1,14 @@
 import { GetServerSideProps } from 'next';
-import Image from 'next/image';
 import { useRouter } from 'next/router';
 import { dehydrate, QueryClient } from 'react-query';
 import ConditionalRender from '../../../components/conditional-render/ConditionalRender';
-import RatingIcons from '../../../components/icons/ratings/RatingIcons';
-import ImageScroll from '../../../components/image/scroll/ImageScroll';
 import AppLayout from '../../../components/layout/app/AppLayout';
 import NavLayout from '../../../components/layout/navigation/NavLayout';
 import ProviderLayout from '../../../components/layout/provider/ProviderLayout.';
+import BusinessInfo from '../../../components/sections/business-info/BusinessInfo.section';
 import useBusiness, {
   fetchBusiness,
 } from '../../../hooks/business/useBusiness';
-import { checkIsOpen } from '../../../utils/api';
-import { truncateText } from '../../../utils/text';
 import { NextPageWithLayout } from '../../_app';
 
 const Business: NextPageWithLayout = () => {
@@ -30,60 +26,7 @@ const Business: NextPageWithLayout = () => {
 
   return (
     <ConditionalRender isLoading={isLoading} isError={isError}>
-      <div className="mt-4 font-rubik">
-        <div className="relative mb-4 h-[200px] w-full">
-          <Image
-            alt={businessData.name}
-            src={businessData.images[0]}
-            layout="fill"
-            objectFit="cover"
-          />
-        </div>
-        <h4 className="mb-2 text-xl font-medium">{businessData.name}</h4>
-        {/* Rating, No.Reviews */}
-        <div className="mb-2 flex items-center gap-4">
-          <RatingIcons avgRating={businessData.avgRating} />
-          <span>{businessData.rating_count} reviews</span>
-          <span className="font-medium">
-            {checkIsOpen(businessData.businessHours) ? 'Open Now' : 'Closed'}
-          </span>
-        </div>
-        {/* Address */}
-        <p className="mb-3 text-secondarytext">
-          {businessData.location.address}
-        </p>
-        {/* Tags */}
-        <div className="mb-3 flex gap-2">
-          <div className="w-max cursor-pointer rounded-sm bg-gray-200 px-3 py-[2px] hover:bg-gray-300">
-            <span className="text-sm capitalize text-secondarytext">
-              {businessData.tags[0]}
-            </span>
-          </div>
-          <div className="w-max cursor-pointer rounded-sm bg-gray-200 px-3 py-[2px] hover:bg-gray-300">
-            <span className="text-sm capitalize text-secondarytext">
-              {businessData.tags[1]}
-            </span>
-          </div>
-        </div>
-        {/* Description */}
-        {/* <p>{businessData.description}</p> */}
-        <p className="mb-7 text-gray-600">
-          {truncateText(
-            'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Enim, nihil dignissimos? Odio vitae dolor quasi eum eos sequi asperiores unde.',
-            20
-          )}
-          <span className="cursor-pointer text-primaryred  hover:text-red-600">
-            {' '}
-            Read More
-          </span>
-        </p>
-        <div>
-          <ImageScroll
-            businessId={businessData._id}
-            images={businessData.images}
-          />
-        </div>
-      </div>
+      <BusinessInfo business={businessData} />
     </ConditionalRender>
   );
 };
