e383ea39f9f972cad4c043e6e49ff265ca2579c5
Prajwal Adhikari <prajwalad101@gmail.com>
2022-07-21 14:59:28 +0545

    feat: implement scroll menu for scrolling images

--- client/components/image/scroll/ImageScroll.tsx
+++ client/components/image/scroll/ImageScroll.tsx
@@ -1,56 +1,95 @@
-import { Types } from 'mongoose';
 import Image from 'next/image';
-import { useRef } from 'react';
+import { useEffect, useRef, useState } from 'react';
 import { BiArrowBack } from 'react-icons/bi';
 import { useContainerDimensions } from '../../../hooks/lib/useContainerDimensions';
+import { classNames } from '../../../utils/tailwind';
 
 export interface ImageScrollProps {
   images: string[];
-  id: Types.ObjectId;
 }
 
-function ImageScroll({ images, id }: ImageScrollProps) {
-  const elementId = id.toString();
-  const componentRef = useRef<HTMLDivElement>(null);
-  const { width: imageWidth } = useContainerDimensions(componentRef);
+function ImageScroll({ images }: ImageScrollProps) {
+  const imageRef = useRef<HTMLDivElement>(null);
+  const imageContainerRef = useRef<HTMLDivElement>(null);
+
+  const { width: imageWidth } = useContainerDimensions(imageRef);
+  const { width: containerWidth } = useContainerDimensions(imageContainerRef);
+  const [scrollPos, setScrollPos] = useState(0);
+  const [scrollStatus, setScrollStatus] = useState<string>('start');
 
   const slideLeft = () => {
-    const slider = document.getElementById(elementId) as HTMLDivElement;
-    slider.scrollLeft = slider.scrollLeft - imageWidth;
+    if (!imageContainerRef.current) {
+      return;
+    }
+
+    // 12 is the padding around container edge (scroll through that space too)
+    const newPosition = scrollPos - containerWidth - 12;
+    imageContainerRef.current.scrollLeft = 0;
+    setScrollPos(newPosition);
   };
 
   const slideRight = () => {
-    const slider = document.getElementById(elementId) as HTMLDivElement;
-    slider.scrollLeft = slider.scrollLeft + imageWidth;
+    if (!imageContainerRef.current) {
+      return;
+    }
+
+    // 12 is the padding around container edge (scroll through that space too)
+    const newPosition = scrollPos + containerWidth + 12;
+    imageContainerRef.current.scrollLeft = newPosition;
+
+    setScrollPos(newPosition);
   };
+
+  // prevent scroll value from getting greater or smaller than scroll menu container
+  useEffect(() => {
+    if (scrollPos <= 0) {
+      setScrollStatus('start');
+    }
+
+    // num of visible images on the screen width
+    const noImagesPerContainer = Math.floor(containerWidth / imageWidth);
+
+    // Math.ceil to account for odd number of images
+    const numContainers = Math.ceil(images.length / noImagesPerContainer);
+
+    const totalWidth = numContainers * containerWidth; // total width of the scroll menu
+
+    const scrollableWidth = totalWidth - containerWidth; // width that can be scrolled
+
+    if (scrollPos >= scrollableWidth) {
+      setScrollStatus('end');
+    }
+  }, [scrollPos, containerWidth, imageWidth, images.length]);
 
   return (
     <div className="relative w-full sm:w-56">
       {/* Left button */}
       <div
-        className="absolute top-[38%] left-2 z-10 cursor-pointer rounded-full bg-white p-2 text-black hover:bg-primaryred hover:text-white"
+        className={classNames(
+          scrollStatus === 'start' ? 'hidden' : 'block',
+          'absolute top-[38%] left-2 z-10 cursor-pointer rounded-full bg-white p-2 text-black hover:bg-primaryred hover:text-white'
+        )}
         onClick={slideLeft}
       >
         <BiArrowBack size={23} />
       </div>
       {/* Right button */}
       <div
-        className="absolute top-[38%] right-2 z-10 cursor-pointer rounded-full bg-white p-2 text-black hover:bg-primaryred hover:text-white"
+        className={classNames(
+          scrollStatus === 'end' ? 'hidden' : 'block',
+          'absolute top-[38%] right-2 z-10 cursor-pointer rounded-full bg-white p-2 text-black hover:bg-primaryred hover:text-white'
+        )}
         onClick={slideRight}
       >
         <BiArrowBack size={23} className="rotate-180" />
       </div>
       {/* Images */}
       <div
-        className="hide-scrollbar relative flex h-[160px] gap-4 overflow-x-hidden scroll-smooth"
-        id={elementId}
+        className="hide-scrollbar relative flex h-[160px] gap-[2%] overflow-x-hidden scroll-smooth"
+        ref={imageContainerRef}
       >
         {images.map((image, index) => (
-          <div
-            key={index}
-            className="relative w-[46%] shrink-0"
-            ref={componentRef}
-          >
+          <div key={index} className="relative w-[49%] shrink-0" ref={imageRef}>
             <Image
               src={image}
               alt="business-image"
--- client/pages/search/business/[businessId].tsx
+++ client/pages/search/business/[businessId].tsx
@@ -62,6 +62,7 @@ const reviewImages = [
   'https://images.unsplash.com/photo-1569718212165-3a8278d5f624?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2080&q=80',
   'https://images.unsplash.com/photo-1562967916-eb82221dfb92?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=686&q=80',
   'https://images.unsplash.com/photo-1562967916-eb82221dfb92?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=686&q=80',
+  'https://images.unsplash.com/photo-1569718212165-3a8278d5f624?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2080&q=80',
 ];
 
 function Reviews({ reviews }: { reviews: IReview[] }) {
@@ -88,7 +89,7 @@ function Reviews({ reviews }: { reviews: IReview[] }) {
             </div>
             <p className="mb-4">{review.review}</p>
             <div className="mb-4">
-              <ImageScroll images={reviewImages} id={review._id} />
+              <ImageScroll images={reviewImages} />
             </div>
             <div className="flex items-center gap-7">
               <div className="mb-4 flex items-center gap-2">
