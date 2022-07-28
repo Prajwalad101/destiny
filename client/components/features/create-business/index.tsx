c43be1c852b7606ddc0a1d30e6530eb9f87702eb
Prajwal Adhikari <prajwalad101@gmail.com>
2022-07-28 12:31:55 +0545

    refactor: change import structure for features

---
+++ client/components/features/create-business/index.tsx
@@ -0,0 +1,6 @@
+import { cardData } from './data/InfoCardData';
+import CreateBusinessHero from './layouts/Hero';
+import EllipsisSeperator from './ui/EllipsisSeperator';
+import InfoCard from './ui/InfoCard';
+
+export { CreateBusinessHero, EllipsisSeperator, InfoCard, cardData };
--- client/pages/for-business/index.tsx
+++ client/pages/for-business/index.tsx
@@ -1,8 +1,10 @@
 import React from 'react';
-import { cardData } from '../../components/features/create-business/data/InfoCardData';
-import CreateBusinessHero from '../../components/features/create-business/layouts/Hero';
-import EllipsisSeperator from '../../components/features/create-business/ui/EllipsisSeperator';
-import InfoCard from '../../components/features/create-business/ui/InfoCard';
+import {
+  cardData,
+  CreateBusinessHero,
+  EllipsisSeperator,
+  InfoCard,
+} from '../../components/features/create-business';
 import AppLayout from '../../components/layout/app/AppLayout';
 import { NextPageWithLayout } from '../_app';
 
