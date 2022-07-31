c62e95a3c53a9e60603f07df2102c66fe477c0a1
Prajwal Adhikari <prajwalad101@gmail.com>
2022-07-31 14:43:49 +0545

    feat: implement form elements

--- client/pages/create-business/register.tsx
+++ client/pages/create-business/register.tsx
@@ -6,6 +6,7 @@ import {
   MyTextInput,
   Navbar,
 } from '../../components/features/create-business';
+import MySelect from '../../components/features/create-business/components/Form/MySelect';
 import AppLayout from '../../components/layout/app/AppLayout';
 import { classNames } from '../../utils/css';
 import { NextPageWithLayout } from '../_app';
@@ -14,6 +15,10 @@ interface MyFormValues {
   businessName: string;
   description: string;
   address: string;
+  businessHours: {
+    openHour: [string, string];
+    closeHour: [string, string];
+  };
 }
 
 const RegisterBusiness: NextPageWithLayout = () => {
@@ -21,6 +26,10 @@ const RegisterBusiness: NextPageWithLayout = () => {
     businessName: '',
     description: '',
     address: '',
+    businessHours: {
+      openHour: ['9', '00'],
+      closeHour: ['21', '00'],
+    },
   };
 
   return (
@@ -30,15 +39,19 @@ const RegisterBusiness: NextPageWithLayout = () => {
         validationSchema={Yup.object({
           businessName: Yup.string()
             .max(40, 'Must be 40 characters or less')
-            .required('Required'),
+            .required('This field is required'),
           description: Yup.string()
             .max(250, 'Must be 250 characters or less')
-            .required('Required'),
+            .required('This field is required'),
           address: Yup.string()
             .max(40, 'Must be 40 characters or less')
-            .required('Required'),
+            .required('This field is required'),
+          businessHours: Yup.object({
+            openWeek: Yup.string().required('This field is required'),
+          }),
         })}
         onSubmit={(values) => {
+          console.log(values);
           alert(JSON.stringify(values, null, 2));
         }}
       >
@@ -48,15 +61,58 @@ const RegisterBusiness: NextPageWithLayout = () => {
   );
 };
 
+const hours = [
+  '1',
+  '2',
+  '3',
+  '4',
+  '5',
+  '6',
+  '7',
+  '8',
+  '9',
+  '10',
+  '11',
+  '12',
+  '13',
+  '14',
+  '15',
+  '16',
+  '17',
+  '18',
+  '19',
+  '20',
+  '21',
+  '22',
+  '23',
+  '24',
+];
+
+const minutes = [
+  '00',
+  '05',
+  '10',
+  '15',
+  '20',
+  '25',
+  '30',
+  '35',
+  '40',
+  '45',
+  '50',
+  '55',
+];
+
 function Step1() {
   return (
     <div className="mt-10 font-rubik">
-      <h1 className="mb-5 text-xl font-medium">
+      <h1 className="mb-5 text-2xl font-medium">
         First,
         <br /> Let&apos;s fill out some basic information
       </h1>
       <ProgressStatus className="mb-10" />
-      <Form>
+
+      <Form className="child-notlast:mb-5">
         <div>
           <MyTextInput
             name="businessName"
@@ -79,13 +135,59 @@ function Step1() {
 
         <div>
           <MyTextInput
-            name="businessName"
+            name="address"
             type="text"
             placeholder="eg: Shrestha Futsal"
             label="Address"
             subLabel="Provide full address of the business location. Make sure the address
             is short and recognizable"
           />
+        </div>
+
+        <div>
+          <label htmlFor="businessHours" className="text-lg font-medium">
+            Business Hours
+          </label>
+          <p className="mt-1 mb-3 text-gray-500">
+            Provide the time of day your business will stay open.
+          </p>
+          <div className="mb-3 flex items-center gap-3">
+            <p className="font-medium">Open Hours</p>
+            <MySelect name="businessHours.openHour[0]" className="w-16">
+              {hours.map((hour) => (
+                <option value={hour} key={hour}>
+                  {hour}
+                </option>
+              ))}
+            </MySelect>
+            <div>:</div>
+            <MySelect name="businessHours.openHour[1]">
+              {minutes.map((minute) => (
+                <option value={minute} key={minute}>
+                  {minute}
+                </option>
+              ))}
+            </MySelect>
+          </div>
+
+          <div className="flex items-center gap-3">
+            <p className="font-medium">Close Hours</p>
+            <MySelect name="businessHours.closeHour[0]" className="w-16">
+              {hours.map((hour) => (
+                <option value={hour} key={hour}>
+                  {hour}
+                </option>
+              ))}
+            </MySelect>
+            <div>:</div>
+            <MySelect name="businessHours.closeHour[1]">
+              {minutes.map((minute) => (
+                <option value={minute} key={minute}>
+                  {minute}
+                </option>
+              ))}
+            </MySelect>
+          </div>
         </div>
 
         <SecondaryButton>
