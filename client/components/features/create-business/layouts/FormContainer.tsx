4137924d9f04d6616cc4b74da61e0dc42cf9e6ce
Prajwal Adhikari <prajwalad101@gmail.com>
2022-08-07 20:29:53 +0545

    feat: implement render props to make listbox reusable

--- client/components/features/create-business/layouts/FormContainer.tsx
+++ client/components/features/create-business/layouts/FormContainer.tsx
@@ -1,12 +1,12 @@
 import { Form } from 'formik';
 import { businessCategories } from '../../../../data/business/categories.data';
+import { classNames } from '../../../../utils/css';
 import SecondaryButton from '../../../button/secondary/SecondaryButton';
 import MyLabel from '../components/Form/MyLabel';
-import MySelect from '../components/Form/MySelect';
 import MySubLabel from '../components/Form/MySubLabel';
 import MyTextArea from '../components/Form/MyTextArea';
 import MyTextInput from '../components/Form/MyTextInput';
-import { hours, minutes } from '../data/form.data';
+import { hours } from '../data/form.data';
 import MyListBox from '../ui/MyListBox/MyListBox';
 
 function FormContainer() {
@@ -19,69 +19,67 @@ function FormContainer() {
         Fill out all the required fields to create your business
       </p>
 
-      <div className="flex flex-col items-end">
-        <Form className="w-2xl child-notlast:mb-5 md:w-full md:child-notlast:mb-16">
-          {/* Business Name */}
-          <FieldLayout>
-            <div className="mb-5">
-              <MyLabel htmlFor="businessName">Name</MyLabel>
-              <MySubLabel className="mb-3">
-                Enter the full name of your business
-              </MySubLabel>
-            </div>
-            <MyTextInput
-              name="businessName"
-              type="text"
-              placeholder="eg: Shrestha Futsal"
-            />
-          </FieldLayout>
+      <Form className="w-2xl child-notlast:mb-5 md:w-full md:child-notlast:mb-16">
+        {/* Business Name */}
+        <FieldLayout>
+          <div className="mb-5">
+            <MyLabel htmlFor="businessName">Name</MyLabel>
+            <MySubLabel className="mb-3">
+              Enter the full name of your business
+            </MySubLabel>
+          </div>
+          <MyTextInput
+            name="businessName"
+            type="text"
+            placeholder="eg: Shrestha Futsal"
+          />
+        </FieldLayout>
 
-          {/* Description */}
-          <FieldLayout>
-            <div className="mb-5">
-              <MyLabel htmlFor="description">Description</MyLabel>
-              <MySubLabel className="mb-3">
-                Enter the description of your business. Tell the customers about
-                your business and include some key features
-              </MySubLabel>
-            </div>
-            <MyTextArea name="description" type="text" rows={5} />
-          </FieldLayout>
+        {/* Description */}
+        <FieldLayout>
+          <div className="mb-5">
+            <MyLabel htmlFor="description">Description</MyLabel>
+            <MySubLabel className="mb-3">
+              Enter the description of your business. Tell the customers about
+              your business and include some key features
+            </MySubLabel>
+          </div>
+          <MyTextArea name="description" type="text" rows={5} />
+        </FieldLayout>
 
-          {/* Address */}
-          <FieldLayout>
-            <div className="mb-5">
-              <MyLabel htmlFor="address">Address</MyLabel>
-              <MySubLabel>
-                Provide full address of your business. Make sure the address is
-                short and recognizable
-              </MySubLabel>
-            </div>
-            <MyTextInput
-              name="address"
-              type="text"
-              placeholder="eg: Naya Baneshwor, Kathmandu"
-            />
-          </FieldLayout>
+        {/* Address */}
+        <FieldLayout>
+          <div className="mb-5">
+            <MyLabel htmlFor="address">Address</MyLabel>
+            <MySubLabel>
+              Provide full address of your business. Make sure the address is
+              short and recognizable
+            </MySubLabel>
+          </div>
+          <MyTextInput
+            name="address"
+            type="text"
+            placeholder="eg: Naya Baneshwor, Kathmandu"
+          />
+        </FieldLayout>
 
-          {/* Business Hours */}
-          <SelectBusinessHours />
+        {/* Business Hours */}
+        <SelectBusinessHours />
 
-          {/* Category */}
-          <FieldLayout>
-            <div className="mb-5">
-              <MyLabel htmlFor="category">Category</MyLabel>
-              <MySubLabel>
-                Select one of the category your business falls on
-              </MySubLabel>
-            </div>
-            <MyListBox list={businessCategories} name="category" />
-          </FieldLayout>
-          <SecondaryButton className="mt-16" type="submit">
-            <p className="px-10 py-2">Submit</p>
-          </SecondaryButton>
-        </Form>
-      </div>
+        {/* Category */}
+        <FieldLayout>
+          <div className="mb-5">
+            <MyLabel htmlFor="category">Category</MyLabel>
+            <MySubLabel>
+              Select one of the category your business falls on
+            </MySubLabel>
+          </div>
+          <MyListBox list={businessCategories} name="category" />
+        </FieldLayout>
+        <SecondaryButton className="mt-16" type="submit">
+          <p className="px-10 py-2">Submit</p>
+        </SecondaryButton>
+      </Form>
     </div>
   );
 }
@@ -95,6 +93,22 @@ const FieldLayout = ({ children }: { children: React.ReactNode }) => {
 };
 
 const SelectBusinessHours = () => {
+  const ListItem = ({
+    children,
+    selected,
+  }: {
+    children: React.ReactNode;
+    selected: boolean;
+  }) => (
+    <span
+      className={classNames(
+        selected ? 'font-medium' : 'font-normal',
+        'capitalize'
+      )}
+    >
+      {children}
+    </span>
+  );
   return (
     <div className="grid-cols-2 gap-10 md:grid lg:grid-cols-[2fr_3fr] lg:gap-24">
       <div className="mb-5">
@@ -107,41 +121,33 @@ const SelectBusinessHours = () => {
         {/* Open */}
         <div className="mb-5 flex items-center gap-3 lg:mb-0">
           <p className="font-medium">Open</p>
-          <MySelect name="businessHours.openHour[0]" className="w-16">
-            {hours.map((hour) => (
-              <option value={hour} key={hour}>
-                {hour}
-              </option>
-            ))}
-          </MySelect>
+          <MyListBox list={hours} width={80} name="businessHours.openHour[0]">
+            {(itemName, selected) => (
+              <ListItem selected={selected}>{itemName}</ListItem>
+            )}
+          </MyListBox>
           <div>:</div>
-          <MySelect name="businessHours.openHour[1]">
-            {minutes.map((minute) => (
-              <option value={minute} key={minute}>
-                {minute}
-              </option>
-            ))}
-          </MySelect>
+          <MyListBox list={hours} width={80} name="businessHours.openHour[1]">
+            {(itemName, selected) => (
+              <ListItem selected={selected}>{itemName}</ListItem>
+            )}
+          </MyListBox>
         </div>
 
         {/* Close */}
         <div className="flex items-center gap-3">
           <p className="font-medium">Close</p>
-          <MySelect name="businessHours.closeHour[0]" className="w-16">
-            {hours.map((hour) => (
-              <option value={hour} key={hour}>
-                {hour}
-              </option>
-            ))}
-          </MySelect>
+          <MyListBox list={hours} width={80} name="businessHours.closeHour[0]">
+            {(itemName, selected) => (
+              <ListItem selected={selected}>{itemName}</ListItem>
+            )}
+          </MyListBox>
           <div>:</div>
-          <MySelect name="businessHours.closeHour[1]">
-            {minutes.map((minute) => (
-              <option value={minute} key={minute}>
-                {minute}
-              </option>
-            ))}
-          </MySelect>
+          <MyListBox list={hours} width={80} name="businessHours.closeHour[1]">
+            {(itemName, selected) => (
+              <ListItem selected={selected}>{itemName}</ListItem>
+            )}
+          </MyListBox>
         </div>
       </div>
     </div>
--- client/components/features/create-business/ui/MyListBox/MyListBox.tsx
+++ client/components/features/create-business/ui/MyListBox/MyListBox.tsx
@@ -6,65 +6,80 @@ import { classNames } from '../../../../../utils/css';
 
 interface MyListBoxProps {
   name?: string;
-  list: { id: number; name: string }[];
+  list: { id?: number; name: string }[];
+  width?: number;
+  children?: (
+    _name: string,
+    _active: boolean,
+    _selected: boolean
+  ) => JSX.Element;
 }
 
-function MyListBox({ name, list }: MyListBoxProps) {
+function MyListBox({ width, name, list, children }: MyListBoxProps) {
   const [selectedItem, setSelectedItem] = useState(list[0]);
 
   return (
-    <div className="w-72 font-rubik">
+    <div className="w-72 font-rubik" style={{ width: width }}>
       <Listbox value={selectedItem} onChange={setSelectedItem} name={name}>
-        <Listbox.Button className="relative w-full rounded-md border-[1px] border-gray-400 px-5 py-2 text-left">
-          <span className="block truncate capitalize">{selectedItem.name}</span>
-          <span className="absolute right-0 top-1/2 -translate-y-1/2 pr-2">
-            <HiOutlineSelector size={20} className="text-gray-400" />
-          </span>
-        </Listbox.Button>
-        <Transition
-          as={Fragment}
-          leave="transition ease-in duration-100"
-          leaveFrom="opacity-100"
-          leaveTo="opacity-0"
-        >
-          <Listbox.Options className="mt-1 max-h-60 rounded-md border-[1px] border-gray-200 py-1 shadow-md">
-            {list.map((listItem) => (
-              <Listbox.Option
-                key={listItem.id}
-                value={listItem}
-                className={({ active }) =>
-                  classNames(
-                    active ? 'bg-blue-500 text-white' : '',
-                    'relative cursor-default py-2 pl-5'
-                  )
-                }
-              >
-                {({ active, selected }) => (
-                  <>
-                    <span
-                      className={classNames(
-                        selected ? 'font-medium' : 'font-normal',
-                        'capitalize'
-                      )}
-                    >
-                      {listItem.name}
-                    </span>
-                    {selected && (
-                      <span className="absolute right-0 top-1/2 -translate-y-1/2 pr-3">
-                        <BsCheck2
-                          size={20}
+        <div className="relative">
+          <Listbox.Button className="relative w-full rounded-md px-5 py-2 text-left ring-1 ring-black/40">
+            <span className="block truncate capitalize">
+              {selectedItem.name}
+            </span>
+            <span className="absolute right-0 top-1/2 -translate-y-1/2 pr-2">
+              <HiOutlineSelector size={20} className="text-gray-400" />
+            </span>
+          </Listbox.Button>
+          <Transition
+            as={Fragment}
+            leave="transition ease-in duration-100"
+            leaveFrom="opacity-100"
+            leaveTo="opacity-0"
+          >
+            <Listbox.Options className="absolute z-10 mt-1 max-h-60 w-full overflow-auto rounded-md bg-white py-1 shadow-lg ring-1 ring-black/5">
+              {list.map((listItem, index) => (
+                <Listbox.Option
+                  key={listItem.id || index}
+                  value={listItem}
+                  className={({ active }) =>
+                    classNames(
+                      active ? 'bg-blue-500 text-white' : '',
+                      'relative cursor-default py-2 pl-5'
+                    )
+                  }
+                >
+                  {({ active, selected }) =>
+                    // implement render props to make listbox accept custom listbox items
+                    children ? (
+                      children(listItem.name, selected, active)
+                    ) : (
+                      <>
+                        <span
                           className={classNames(
-                            active ? 'text-white' : 'text-blue-700'
+                            selected ? 'font-medium' : 'font-normal',
+                            'capitalize'
                           )}
-                        />
-                      </span>
-                    )}
-                  </>
-                )}
-              </Listbox.Option>
-            ))}
-          </Listbox.Options>
-        </Transition>
+                        >
+                          {listItem.name}
+                        </span>
+                        {selected && (
+                          <span className="absolute right-0 top-1/2 -translate-y-1/2 pr-3">
+                            <BsCheck2
+                              size={20}
+                              className={classNames(
+                                active ? 'text-white' : 'text-blue-700'
+                              )}
+                            />
+                          </span>
+                        )}
+                      </>
+                    )
+                  }
+                </Listbox.Option>
+              ))}
+            </Listbox.Options>
+          </Transition>
+        </div>
       </Listbox>
     </div>
   );
