359849c53a9795d4d2298983d2994e82e9048d45
Prajwal Adhikari <prajwalad101@gmail.com>
2022-08-19 16:30:44 +0545

    refactor: implement queryclient provider for create-business

--- client/components/layout/provider/ProviderLayout..tsx
+++ client/components/layout/provider/ProviderLayout..tsx
@@ -4,7 +4,7 @@ import { ReactQueryDevtools } from 'react-query/devtools';
 interface IProviderLayout {
   children: React.ReactNode;
   // eslint-disable-next-line @typescript-eslint/no-explicit-any
-  pageProps: any;
+  pageProps?: any;
 }
 
 const queryClient = new QueryClient({
@@ -16,9 +16,16 @@ const queryClient = new QueryClient({
 });
 
 function ProviderLayout({ children, pageProps }: IProviderLayout) {
+  let component;
+  if (pageProps) {
+    component = <Hydrate state={pageProps.dehydratedState}>{children}</Hydrate>;
+  } else {
+    component = children;
+  }
+
   return (
     <QueryClientProvider client={queryClient}>
-      <Hydrate state={pageProps.dehydratedState}>{children}</Hydrate>
+      {component}
       <ReactQueryDevtools initialIsOpen={false} />
     </QueryClientProvider>
   );
