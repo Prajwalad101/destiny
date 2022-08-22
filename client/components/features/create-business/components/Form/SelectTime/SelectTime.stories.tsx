9a5e9493bfcdf0cff289649a7caa06e96f6bb118
Prajwal Adhikari <prajwalad101@gmail.com>
2022-08-22 10:57:46 +0545

    fix: prevent incorrect imports and story implementation

--- client/components/features/create-business/components/Form/SelectTime/SelectTime.stories.tsx
+++ client/components/features/create-business/components/Form/SelectTime/SelectTime.stories.tsx
@@ -1,8 +1,5 @@
-import { formData } from '@features/create-business';
 import { ComponentMeta, ComponentStory } from '@storybook/react';
 import SelectTime from './SelectTime';
-
-const { hours, minutes, timeOfDay } = formData;
 
 export default {
   title: 'components/CombinedListBox',
@@ -14,9 +11,3 @@ const Template: ComponentStory<typeof SelectTime> = (args) => (
 );
 
 export const Base = Template.bind({});
-
-Base.args = {
-  hours,
-  minutes,
-  timeOfDay,
-};
