043c88b6ad90de9aa263074695a1d30575cc38a8
Prajwal Adhikari <prajwalad101@gmail.com>
2022-09-06 10:49:57 +0545

    refactor: implement custom hook to get window dimensions

--- client/hooks/index.ts
+++ client/hooks/index.ts
@@ -3,10 +3,13 @@ import useBusinesses from './business/useBusinesses';
 import useEffectAfterMount from './useEffectAfterMount';
 import useIsomorphicLayoutEffect from './useIsomorphicLayoutEffect';
 import usePreventBodyOverflow from './usePreventBodyOverflow';
+import useWindowSize from './useWindowSize';
+
 export {
   useBusiness,
   useBusinesses,
   useEffectAfterMount,
   useIsomorphicLayoutEffect,
   usePreventBodyOverflow,
+  useWindowSize,
 };
