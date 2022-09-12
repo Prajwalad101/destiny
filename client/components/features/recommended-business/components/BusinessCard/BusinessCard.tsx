eaca035434e0eb703006af46078a701d9b5a7ec2
Prajwal Adhikari <prajwalad101@gmail.com>
2022-09-12 21:25:01 +0545

    refactor(UI): make slight UI changes to business card

--- client/components/features/recommended-business/components/BusinessCard/BusinessCard.tsx
+++ client/components/features/recommended-business/components/BusinessCard/BusinessCard.tsx
@@ -11,14 +11,14 @@ function BusinessCard({ business }: BusinessCardProps) {
   const images = business.images.map((image) => getPublicFilePath(image));
 
   return (
-    <div className="cursor-pointer font-rubik">
+    <div className="transition-color group cursor-pointer rounded-md font-rubik hover:bg-gray-50">
       <div className="relative h-[200px] sm:h-[250px]">
         <Image
           src={images[0]}
           alt={business.name}
           layout="fill"
           objectFit="cover"
-          className="rounded-t-sm hover:opacity-90"
+          className="rounded-t-sm transition-opacity group-hover:opacity-90"
         />
       </div>
 
@@ -27,14 +27,6 @@ function BusinessCard({ business }: BusinessCardProps) {
         <div className="mb-1">
           <p className="text-base font-medium sm:text-lg">{business.name}</p>
         </div>
-        {/* {props.status ? (
-          <div className="mb-3 w-max rounded-sm bg-[#DDE7CA] px-2 py-[2px]">
-            <p className="text-sm capitalize text-secondarytext">
-              {props.status}
-            </p>
-          </div>
-        ) : null} */}
-
         {/* Business Rating */}
         <div className="mb-2 flex gap-2 ">
           <RatingIcons avgRating={business.avgRating} />
@@ -44,7 +36,7 @@ function BusinessCard({ business }: BusinessCardProps) {
         </div>
 
         {/* Location */}
-        <p className="mb-1 overflow-hidden text-ellipsis whitespace-nowrap text-sm">
+        <p className="mb-1 overflow-hidden text-ellipsis whitespace-nowrap pb-5 text-sm">
           {business.location.address}
         </p>
       </div>
