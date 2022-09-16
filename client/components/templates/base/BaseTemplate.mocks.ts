98b9118c17517965dfe9062c057915f40e0c6d44
Prajwal Adhikari <prajwalad101@gmail.com>
2022-09-16 19:04:12 +0545

    refactor: cleanup unnecessary component files

--- client/components/templates/base/BaseTemplate.mocks.ts
+++
@@ -1,9 +0,0 @@
-import { IBaseTemplate } from './BaseTemplate';
-
-const base: IBaseTemplate = {
-  sampleText: 'Hello world!',
-};
-
-export const mockBaseTemplateProps = {
-  base,
-};
--- client/components/templates/base/BaseTemplate.tsx
+++
@@ -1,9 +0,0 @@
-export interface IBaseTemplate {
-  sampleText: string;
-}
-
-function BaseTemplate({ sampleText }: IBaseTemplate) {
-  return <div>{sampleText}</div>;
-}
-
-export default BaseTemplate;
