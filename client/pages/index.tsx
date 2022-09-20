a242d7cc0ce2d5425a2016ddc4779a2f4da1d773
Prajwal Adhikari <prajwalad101@gmail.com>
2022-09-20 21:18:52 +0545

    refactor: modify home-page for cleaner component structure

--- client/pages/index.tsx
+++ client/pages/index.tsx
@@ -1,14 +1,50 @@
-import { HeroSection } from '@features/home-page/layouts';
+import {
+  CategoriesDropdown,
+  MainHeading,
+  Searchbar,
+} from '@features/home-page/components';
 import { RecommendedSection } from '@features/recommended-business/layouts';
 import { QueryProvider } from 'components/context-provider';
 import { NavigationProvider } from 'components/context-provider/NavigationProvider/NavigationProvider';
+import { AppLayout } from 'components/layout';
+import { Navbar } from 'components/navigation';
 import Sidebar from 'components/navigation/sidebar/Sidebar';
+import { businessCategoriesData } from 'data';
 import { NextPageWithLayout } from 'pages/_app';
 
 const Home: NextPageWithLayout = () => {
   return (
     <div>
-      <HeroSection />
+      <div className="relative mb-10 md:mb-0 md:h-[700px]">
+        {/* BG Image */}
+        <div className="absolute inset-0 -z-10 hidden bg-main-img bg-cover bg-no-repeat md:block" />
+        {/* Navbar */}
+        <Navbar theme="dark" size="sm" />
+        <AppLayout size="sm">
+          <section className="mt-7 md:mt-20">
+            <MainHeading className="mb-5 max-w-sm sm:max-w-xl md:mb-7">
+              Find and support local businesses
+            </MainHeading>
+            <div className="w-full max-w-xl md:max-w-2xl">
+              <Searchbar
+                foodPlaceholder="Search for stuff"
+                locationPlaceholder="Kathmandu, New baneshwor"
+              />
+            </div>
+            {/* Dropdown Items */}
+            <div className="mt-5 hidden gap-5 font-rubik md:flex">
+              {businessCategoriesData.map((data, index) => (
+                <div key={index}>
+                  <CategoriesDropdown
+                    name={data.name}
+                    subcategories={data.subcategories}
+                  />
+                </div>
+              ))}
+            </div>
+          </section>
+        </AppLayout>
+      </div>
       <div>
         <RecommendedSection
           title="Trending right now"
