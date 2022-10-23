61383f546de1a7cc2cf97922fc75db3299500499
Prajwalad101 <prajwalad101@gmail.com>
2022-10-23 14:27:48 +0545

    refactor: add responsiveness to loading-skeleton

--- client/features/business-details/components/ReviewSkeleton/ReviewSkeleton.tsx
+++ client/features/business-details/components/ReviewSkeleton/ReviewSkeleton.tsx
@@ -6,14 +6,14 @@ export default function ReviewSkeleton() {
       {cards.map((card, i) => (
         <div
           key={i}
-          className="animate mb-7 flex h-56 flex-col rounded-lg bg-gray-200/60 p-6"
+          className="animate mb-4 flex flex-col rounded-md bg-gray-200/60 px-6 py-8 xs:mb-6"
         >
-          <div className="mb-8 flex items-center gap-5">
-            <div className="h-12 w-12 rounded-full bg-white" />
-            <div className="h-4 w-28 rounded-xl bg-white" />
+          <div className="mb-5 flex items-center gap-5 xs:mb-8">
+            <div className="h-10 w-10 shrink-0 rounded-full bg-white xs:h-12 xs:w-12" />
+            <div className="h-4 w-28 rounded-xl bg-white xs:block" />
           </div>
-          <div className="mb-5 h-4 w-full rounded-sm bg-white" />
-          <div className="mb-5 h-4 w-full rounded-sm bg-white" />
+          <div className="mb-3 h-4 w-full rounded-sm bg-white xs:mb-5" />
+          <div className="mb-3 h-4 w-full rounded-sm bg-white xs:mb-5" />
           <div className="h-4 w-full rounded-sm bg-white" />
         </div>
       ))}
