df5888611e0b73d2662bbbf177105f335fe5ae75
Prajwal Adhikari <prajwalad101@gmail.com>
2022-11-16 07:39:20 +0545

    refactor: move component into its own file

--- client/src/features/register-business/layouts/index.ts
+++ client/src/features/register-business/layouts/index.ts
@@ -1,6 +1,5 @@
 import FormStep1 from '../components/FormStep1/FormStep1';
-import FieldLayout from './FieldLayout/FieldLayout';
 import FormContainer from './FormContainer/FormContainer';
 import HeroSection from './HeroSection/HeroSection';
 
-export { FieldLayout, FormContainer, HeroSection, FormStep1 };
+export { FormContainer, HeroSection, FormStep1 };
